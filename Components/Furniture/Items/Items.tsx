85907a199cfc15f29f620dc477217d0d431a8ef6
Abhinav TV <cworled6028@gmail.com>
2023-04-19 13:09:29 +0530

    some chnagesare there

--- Components/Furniture/Items/Items.tsx
+++ Components/Furniture/Items/Items.tsx
@@ -7,52 +7,79 @@ export default function Items() {
     const items = [
         {
             item: '1',
-            color: 'red'
+            color: '#E1C7A0',
+            imageUrl: '/assets/furniture/items/chair.png'
         },
         {
             item: '2',
-            color: 'blue'
+            color: 'blue',
+            imageUrl: '/assets/furniture/items/table.png'
         },
         {
             item: '2',
-            color: 'green'
+            color: 'green',
+            imageUrl: '/assets/furniture/items/chair.png'
         },
         {
             item: '1',
-            color: 'yellow'
+            color: 'yellow',
+            imageUrl: '/assets/furniture/items/chair.png'
         },
         {
             item: '1',
-            color: 'orange'
+            color: 'orange',
+            imageUrl: '/assets/furniture/items/chair.png'
         },
         {
             item: '1',
-            color: 'green'
+            color: 'green',
+            imageUrl: '/assets/furniture/items/chair.png'
         },
         {
             item: '1',
-            color: 'orange'
+            color: 'orange',
+            imageUrl: '/assets/furniture/items/chair.png'
         },
         {
             item: '1',
-            color: 'green'
-        },
+            color: 'orange',
+            imageUrl: '/assets/furniture/items/chair.png'
+        }
     ]
-
+    // bgcolor={MAIN_C0LOUR}
     return (
-        <Grid container item bgcolor="  " justifyContent="center" >
+        <Grid container item justifyContent="center" sx={{
+            bgcolor: { xs: 'red', sm: 'blue', md: 'green', lg: 'yellow' }
+        }}>
 
-            <Grid item container bgcolor="red" lg={11 }  >
-            {/* mx={4} */}
+            <Grid item container bgcolor="" lg={11.5}  >
+
                 {
                     items.map((data: any, index: any) =>
 
                         <Grid item container key={index} xs={6} md={4} lg={3} justifyContent="center">
 
-                            <Grid container item bgcolor="pink" my={1} xs={10} lg={10} justifyContent="center">
+                            <Grid container item bgcolor={data.color} my={1} xs={10} lg={9} justifyContent="center" mt={5} sx={{
+                                height: { xs: 250, sm: 400, lg: 400 }
+                            }}>
 
-                                ftjh
-                                
+                                <Box
+                                    component="img"
+                                    sx={{
+                                        mt: { xs: 3, lg: 7 },
+                                        height: 200,
+                                        width: 150,
+                                        maxHeight: { xs: 120, sm: 280, md: '150' },
+                                        maxWidth: { xs: 80, sm: 140, md: '120' },
+                                    }}
+                                    alt="The house from the offer."
+                                    src={data.imageUrl}
+                                />
+
+                                <Grid item container sx={{ bgcolor: 'red', justifyContent: 'center' }}>
+                                    A
+                                </Grid>
+
                             </Grid>
 
                         </Grid>
@@ -65,17 +92,3 @@ export default function Items() {
         </Grid>
     )
 }
-
-// <Grid container >
-//     {
-//         items.map((data: any, index: any) =>
-
-//             <Grid container bgcolor={data.color} key={index} xs={6} md={4} lg={3} sx={{}} >
-
-//                 {data.item}
-
-//             </Grid>
-
-//         )
-//     }
-// </Grid>
